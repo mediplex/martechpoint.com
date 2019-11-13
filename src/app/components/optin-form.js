94b3c6b0544b468e3f1a78fed461953aee30cfcc
Mehdi Karim <contact@mehdikarim.com>
2019-11-13 20:27:01 +0000

    feat(optin-form): persist data in firestore

---
+++ src/app/components/optin-form.js
@@ -0,0 +1,110 @@
+import { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+import { Box, TextField, Button, Typography } from '@material-ui/core';
+
+import LoopIcon from '@material-ui/icons/Loop';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+const ButtonIndicator = ({ isSubmitting }) => {
+  if (!isSubmitting) return <ArrowForwardIcon />;
+  else
+    return (
+      <>
+        <LoopIcon className="loading" />
+        <style jsx global>{`
+          .loading {
+            animation: rotation 4s infinite linear;
+          }
+
+          @keyframes rotation {
+            from {
+              transform: rotate(0deg);
+            }
+            to {
+              transform: rotate(-359deg);
+            }
+          }
+        `}</style>
+      </>
+    );
+};
+
+export const OptinForm = ({ listId }) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const initialValues = {
+    email: ''
+  };
+
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .required()
+  });
+
+  const handleSubmit = async values => {
+    setIsSubmitting(true);
+
+    const firestore = await import('../firebase').then(({ firestore }) => firestore);
+
+    await firestore
+      .collection('lists')
+      .doc(listId)
+      .set({
+        email: values.email,
+        optin: Date.now()
+      })
+      .then(() => {
+        setIsSubmitting(false);
+        console.info('Success ✔');
+      })
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <Formik initialValues={initialValues} validationSchema={schema} onSubmit={values => handleSubmit(values)}>
+      {/* {({ errors, touched }) => ( */}
+      <Form>
+        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'stretch'}>
+          <Field name={'email'}>
+            {({
+              field, // { name, value, onChange, onBlur }
+              form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+              meta
+            }) => (
+              <TextField
+                helperText={touched['email'] ? errors['email'] : ''}
+                error={touched['email'] && Boolean(errors['email'])}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                placeholder={'Your Best Email Address'}
+                label={'Email Address'}
+                {...field}
+              />
+            )}
+          </Field>
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="secondary"
+            startIcon={<ButtonIndicator isSubmitting={isSubmitting} />}
+            style={{ minHeight: 56 }}
+            disabled={isSubmitting}
+          >
+            I Want 50% OFF
+          </Button>
+
+          <Typography variant={'caption'} align={`center`}>
+            Sign up now to get early notification of our launch date!
+          </Typography>
+        </Box>
+      </Form>
+      {/* )} */}
+    </Formik>
+  );
+};
+export default OptinForm;
--- src/functions/index.js
+++ src/functions/index.js
@@ -19,3 +19,40 @@ exports.static = onRequest((request, response) => {
   response.set('Cache-Control', `public, max-age=${60 * 60 * 24 * 365}, smaxage=${60 * 60 * 24 * 365}`);
   response.sendFile(`${__dirname}${request.path.replace('_next', '.next')}`);
 });
+
+// Listen for changes in all documents in the 'users' collection
+// exports.sendEmail = functions.firestore.document('lists/{listId}').onUpdate((change, context) => {
+//   // If we set `/users/marie` to {name: "Marie"} then
+//   // context.params.userId == "marie"
+//   // ... and ...
+//   // change.after.data() == {name: "Marie"}
+
+//   const nodemailer = require('nodemailer');
+//     let transporter = nodemailer.createTransport({
+//         sendmail: true,
+//         newline: 'windows',
+//         logger: false
+//     });
+
+//     // Message object
+//     let message = {
+//         from: 'Mehdi from Martech Point<mehdi.karim@martechpoint.com>',
+
+//         // Comma separated list of recipients
+//         to: context.params.email.email,
+//         bcc: 'mehdi.karim@outlook.com',
+
+//         // Subject of the message
+//         subject: '🔴 Nodemailer Test Email ✔',
+
+//         // plaintext body
+//         text: 'Hello to myself!',
+
+//         // HTML body
+//         html: '<p><b>Hello</b> to myself </p>'
+//      };
+
+//     let info = await transporter.sendMail(message);
+//     console.log('Message sent successfully as %s', info.messageId);
+
+// });
