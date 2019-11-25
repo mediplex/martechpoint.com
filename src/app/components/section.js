ee4b49e113dc6aaaead9938daa285cd8318cc1de
Mehdi Karim <contact@mehdikarim.com>
2019-11-25 14:29:40 +0000

    improvement(section): animate and make section header responsive

--- src/app/components/section.js
+++ src/app/components/section.js
@@ -1,29 +1,47 @@
-import { Box, Container, Typography } from '@material-ui/core';
+import { useRef, useEffect, useState } from 'react';
+import { AppBar, Box, Container, Typography, useScrollTrigger } from '@material-ui/core';
 
-export const Section = ({ children, title }) => (
-  <Box component={`section`}>
-        <Box
-        style={{
-          position: 'sticky',
-          top: 0,
-          left: 0,
-          zIndex: 999 //:not(:stuck) // should get smaller with a smooth trasition
-        }}
-        bgcolor={`#4CAF4F`}
-        color={`#FFF`}
-        py={{
-          xs: 5,
-          sm: 5,
-          md: 5,
-          lg: 5,
-          xl: 5,
-        }}>
+const ElevationScroll = ({ children }) => {
+  const [threshold, setThreshold] = useState(0)
+
+  useEffect(()=>{
+    if (children.ref.current){
+      setThreshold(children.ref.current.getBoundingClientRect().y)
+    }
+  },[children.ref.current])
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: threshold
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+    // style: trigger? {paddingTop: 5, paddingBottom: 5 } : {paddingTop: 15, paddingBottom: 15 }
+  });
+};
+
+export const Section = ({ children, title }) => {
+  const sectionHeader = useRef();
+
+  return (
+    <Box component={`section`}>
+      <ElevationScroll>
+        <AppBar
+          position="sticky"
+          ref={sectionHeader}
+        >
           <Container maxWidth={`lg`}>
-          <Typography variant={`h3`} align={`center`}>
-            {title}
-          </Typography>
+            <Box fontSize={{xs: 'h4.fontSize', lg: 'h3.fontSize'}}  align={`center`} py={{xs: 1, lg: 2}}>
+            {/* <Typography > */}
+              {title}
+            {/* </Typography> */}
+
+            </Box>
           </Container>
-        </Box>
+        </AppBar>
+      </ElevationScroll>
+
 
       <Box
         display="flex"
@@ -35,21 +53,31 @@ export const Section = ({ children, title }) => (
           sm: 5,
           md: 5,
           lg: 5,
-          xl: 5,
+          xl: 5
         }}
         paddingBottom={{
           xs: 15,
           sm: 15,
           md: 15,
           lg: 15,
-          xl: 15,
+          xl: 15
         }}
       >
-              <Container maxWidth={'md'}>
-        {children}
-    </Container>
+        <Container maxWidth={'md'}>{children}</Container>
       </Box>
-  </Box>
-);
 
+      {/* <style jsx global>
+      {`
+       header.section-header { 
+          background: red
+      }
+
+        div.section-header:stuck {
+          background-color: red
+        }
+    `}
+    </style> */}
+    </Box>
+  );
+};
 export default Section;
